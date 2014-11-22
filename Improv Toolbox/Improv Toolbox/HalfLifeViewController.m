fbcd32edc5c0700871cd1a297982a65b332c39f5
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2014-11-22 15:22:05 -0500

    working on half life timers now

--- Improv Toolbox/Improv Toolbox/HalfLifeViewController.m
+++ Improv Toolbox/Improv Toolbox/HalfLifeViewController.m
@@ -7,15 +7,21 @@
 //
 
 #import "HalfLifeViewController.h"
+#import <iAd/iAd.h>
 
-@interface HalfLifeViewController ()
+@interface HalfLifeViewController () <ADBannerViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 - (IBAction)startTimer:(id)sender;
 - (IBAction)halfTimer:(id)sender;
 - (IBAction)resetButton:(id)sender;
 
 @property (strong, nonatomic) NSTimer *timer;
-@property double timerValue;
+@property float timerValue;
+@property int timerCursor;
+@property (nonatomic) BOOL pauseTimeCounting;
+@property (strong, nonatomic) NSArray *timerValues;
+
+@property (weak, nonatomic) IBOutlet ADBannerView *adView;
 
 @end
 
@@ -24,7 +30,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.timerValue = 60.0;
+    self.timerValues = @[ @60.0f, @30.0f, @15.0f, @7.5f, @3.0f, @1.5f];
+    self.timerCursor = 0;
+    self.timerValue = [[self.timerValues objectAtIndex:self.timerCursor] floatValue];
+    
+    //set up the iAd, make this the delegate
+    self.adView.delegate = self;
+    // Initially hide the ad banner.
+    self.adView.alpha = 0.0;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,37 +51,49 @@
                                                 selector:@selector(updateTimerLabel:)
                                                 userInfo:nil
                                                  repeats:YES];
+    self.pauseTimeCounting = NO;
 }
 
 - (IBAction)halfTimer:(id)sender {
     [self killTimer];
     self.timerValue /= 2;
-    [self.timeLabel setText:[NSString stringWithFormat:@"%f", self.timerValue]];
+    [self updateTimerLabel];
 }
 
 - (IBAction)resetButton:(id)sender {
     [self killTimer];
     [self resetTimerValue];
-    [self.timeLabel setText:[NSString stringWithFormat:@"%f", self.timerValue]];
+    [self updateTimerLabel];
 }
 
 - (void)resetTimerValue
 {
     [self killTimer];
     self.timerValue = 60.0;
-    [self.timeLabel setText:[NSString stringWithFormat:@"%f", self.timerValue]];
+    
 }
 
 - (void)updateTimerLabel:(id)sender
 {
-    [self.timeLabel setText:[NSString stringWithFormat:@"%f", self.timerValue]];
+    if (!self.pauseTimeCounting) {
+        self.timerValue++;
+        [self updateTimerLabel];
+    }
+    
+    
 }
 
 - (void)killTimer{
     if(self.timer){
         [self.timer invalidate];
         self.timer = nil;
+        self.pauseTimeCounting = YES;
     }
+}
+
+- (void)updateTimerLabel
+{
+    [self.timeLabel setText:[NSString stringWithFormat:@"%f", self.timerValue]];
 }
 
 @end
