6c86a74fc8f76b37e8186f0c89b540cc8896663e
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2014-12-13 22:21:56 -0800

    fixed the sounds playing in background and on/off toggle for beat sounds, plus playing sounds over beat sounds

--- Improv Toolbox/Improv Toolbox/SoundboardViewController.m
+++ Improv Toolbox/Improv Toolbox/SoundboardViewController.m
@@ -12,7 +12,8 @@
 
 @interface SoundboardViewController () <AVAudioPlayerDelegate, ADBannerViewDelegate>
 
-@property AVAudioPlayer *player;
+@property AVAudioPlayer *effectPlayer;
+@property AVAudioPlayer *beatPlayer;
 @property float volume;
 
 - (IBAction)yaySound:(id)sender;
@@ -27,6 +28,8 @@
 - (IBAction)beat1Sound:(id)sender;
 - (IBAction)beat2Sound:(id)sender;
 - (IBAction)beat3Sound:(id)sender;
+
+@property (nonatomic) BOOL beatSoundPlaying;
 
 @property (weak, nonatomic) IBOutlet ADBannerView *adView;
 @property (nonatomic, strong) NSTimer *adTimer;
@@ -59,8 +62,8 @@
     //set up the audio thing
     NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"airhorn" ofType:@"mp3"];
     NSURL *url = [NSURL fileURLWithPath:soundPath];
-    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
-    [self.player setDelegate:self];
+    self.effectPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
+    [self.effectPlayer setDelegate:self];
     
     //set up volume for the sounds
     //default 1.0
@@ -69,9 +72,92 @@
     
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
-    //stop the sound from playing
-    [self.player stop];
+//- (void)viewWillDisappear:(BOOL)animated {
+//    //stop the sound from playing
+//    [self.effectPlayer stop];
+//    [self.beatPlayer stop];
+//}
+
+#pragma mark - Sound playing methods
+
+- (IBAction)yaySound:(id)sender {
+    [self playSound:@"yay"];
+}
+
+- (IBAction)airhornSound:(id)sender {
+    [self playSound:@"airhorn"];
+}
+
+- (IBAction)knockSound:(id)sender {
+    [self playSound:@"knock"];
+}
+
+- (IBAction)dingSound:(id)sender {
+    [self playSound:@"ding"];
+}
+
+- (IBAction)buzzerSound:(id)sender {
+    [self playSound:@"buzzer"];
+}
+
+- (IBAction)countdownSound:(id)sender {
+    [self playSound:@"countdown"];
+}
+
+- (IBAction)phone1Sound:(id)sender {
+    [self playSound:@"phone1"];
+}
+
+- (IBAction)phone2Sound:(id)sender {
+    [self playSound:@"phone2"];
+}
+
+- (IBAction)doorbellSound:(id)sender {
+    [self playSound:@"doorbell"];
+}
+
+- (IBAction)beat1Sound:(id)sender {
+    [self playBeatSound:@"beat1"];
+}
+
+- (IBAction)beat2Sound:(id)sender {
+    [self playBeatSound:@"beat2"];
+}
+
+- (IBAction)beat3Sound:(id)sender {
+    [self playBeatSound:@"beat3"];
+}
+
+- (void)playSound:(NSString *)trackTitle
+{
+    NSString *soundPath = [[NSBundle mainBundle] pathForResource:trackTitle ofType:@"mp3"];
+    NSURL *url = [NSURL fileURLWithPath:soundPath];
+    self.effectPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
+    [self.effectPlayer setDelegate:self];
+    [self.effectPlayer setVolume:1.0];
+    [self.effectPlayer play];
+}
+
+- (void)playBeatSound:(NSString *)trackTitle
+{
+    if (!self.beatSoundPlaying) {
+        //committing bad OOP but #yolo 
+        NSString *soundPath = [[NSBundle mainBundle] pathForResource:trackTitle ofType:@"mp3"];
+        NSURL *url = [NSURL fileURLWithPath:soundPath];
+        self.beatPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
+        [self.beatPlayer setDelegate:self];
+        [self.beatPlayer setVolume:1.0];
+        [self.beatPlayer play];
+        
+        self.beatSoundPlaying = TRUE;
+    }
+    else {
+        [self.beatPlayer stop];
+        self.beatSoundPlaying = FALSE;
+    }
+}
+
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
 }
 
 #pragma mark - iAd methods
@@ -114,66 +200,4 @@
         self.secondsElapsed++;
 }
 
-#pragma mark - Sound playing methods
-
-- (IBAction)yaySound:(id)sender {
-    [self playSound:@"yay"];
-}
-
-- (IBAction)airhornSound:(id)sender {
-    [self playSound:@"airhorn"];
-}
-
-- (IBAction)knockSound:(id)sender {
-    [self playSound:@"knock"];
-}
-
-- (IBAction)dingSound:(id)sender {
-    [self playSound:@"ding"];
-}
-
-- (IBAction)buzzerSound:(id)sender {
-    [self playSound:@"buzzer"];
-}
-
-- (IBAction)countdownSound:(id)sender {
-    [self playSound:@"countdown"];
-}
-
-- (IBAction)phone1Sound:(id)sender {
-    [self playSound:@"phone1"];
-}
-
-- (IBAction)phone2Sound:(id)sender {
-    [self playSound:@"phone2"];
-}
-
-- (IBAction)doorbellSound:(id)sender {
-    [self playSound:@"doorbell"];
-}
-
-- (IBAction)beat1Sound:(id)sender {
-    [self playSound:@"beat1"];
-}
-
-- (IBAction)beat2Sound:(id)sender {
-    [self playSound:@"beat2"];
-}
-
-- (IBAction)beat3Sound:(id)sender {
-    [self playSound:@"beat3"];
-}
-
-- (void)playSound:(NSString *)trackTitle
-{
-    NSString *soundPath = [[NSBundle mainBundle] pathForResource:trackTitle ofType:@"mp3"];
-    NSURL *url = [NSURL fileURLWithPath:soundPath];
-    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
-    [self.player setDelegate:self];
-    [self.player setVolume:1.0];
-    [self.player play];
-}
-
-- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
-}
 @end
